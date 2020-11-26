99136e0e63f72d4c93c45588aafd8e74c1e3f1f4
shweta2902 <74788666+shweta2902@users.noreply.github.com>
2020-11-27 01:41:44 +0530

    Add files via upload

---
+++ Project 21/sketch.js
@@ -0,0 +1,48 @@
+
+var wall,thickness;
+var bullet,speed,weight;
+ 
+
+function setup() {
+  createCanvas(1600,400);
+  thickness=random(22,93);
+  speed=random(223,321);
+  weight=random(30,52);
+  bullet=createSprite(40, 200, 80, 20);
+  wall=createSprite(1500, 200, thickness, height/2)
+  wall.shapeColor=color(80,80,80)
+  bullet.velocityX = speed;
+}
+
+function draw() {
+  background(255,255,255); 
+  if(hascollided(bullet,wall))
+  
+  {
+    bullet.velocityX = 0;
+    var damage = 0.5 * weight * speed * speed/(thickness * thickness *thickness);
+
+    if(damage >10)
+    {
+      wall.shapeColor=color(255,0,0);
+      
+    }
+    if(damage < 10)
+    {
+      wall.shapeColor=color(0,255,0);
+      
+    }
+  }
+  drawSprites();
+}
+
+function hascollided(lbullet,lwall)
+{
+  bulletRightEdge =lbullet.x + lbullet.width;
+  wallLeftEdge = lwall.x;
+  if(bulletRightEdge >= wallLeftEdge)
+  {
+    return true;
+  }
+  return false;
+}
